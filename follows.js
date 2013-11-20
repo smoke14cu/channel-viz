082f1ce9e050ec947b51730df23d66e7f386e33f
smoke14cu <aclark@sensortechcorp.com>
2013-11-20 08:45:52 -0500

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,8 +12,8 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
+	var defaultKey		= 'wJ2wLFXP5rdboxUOoCE4KylqnQCBlUkpxRQh6T7BROGfcIcr', // Unique master Xively API key to be used as a default
+		defaultFeeds	= ['1321106950'], // Comma separated array of Xively Feed ID numbers
 		applicationName	= '', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
